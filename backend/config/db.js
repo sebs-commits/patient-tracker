f4ab91b2372fc90151e01dfe0572a36ac61788b4
sebdavid <121909326+sebs-commits@users.noreply.github.com>
2024-09-11 16:56:04 -0400

    Removed depracated option

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB connected with Mongoose...");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Successfully connected to your Mongo Database!");
   } catch (err) {
     console.error("MongoDB connection failed:", err.message);
     process.exit(1);
