8f1263716f1d7a778b5bc0ff671bb8884a000c8d
sebdavid <121909326+sebs-commits@users.noreply.github.com>
2024-09-17 11:40:14 -0400

    added layout directory, moved navbar

---
+++ frontend/src/components/layout/NavBar.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <Link to="/add-patient">Add Patient</Link>
+      <Link to="/patients">View Patients</Link>
+    </nav>
+  );
+};
+
+export default NavBar;
