df900f217c8b75215079ae7e6104cd7ad627256d
sebdavid <121909326+sebs-commits@users.noreply.github.com>
2024-09-25 19:47:54 -0400

    imported navbar to app.jsx, added conditional rendering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,71 +1,27 @@
-//import "./styles/App.css";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import PropTypes from "prop-types";
-import AddPatients from "./components/pages/AddPatients";
-import PatientList from "./components/pages/PatientList";
-import PatientDetail from "./components/pages/PatientDetail";
-import Login from "./components/pages/Login";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import NavBar from "./components/layout/NavBar";
 import Dashboard from "./components/pages/Dashboard";
+import TransporterDashboard from "./components/pages/TransporterDashboard";
+import Login from "./components/pages/Login";
 import Register from "./components/pages/Register";
+// Import other components as needed
 
-const ProtectedRoute = ({ children }) => {
-  const token = localStorage.getItem("token");
-  if (!token) {
-    return <Navigate to="/login" />;
-  }
-  return children;
-};
-
-ProtectedRoute.propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
-const App = () => {
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/add-patient"
-          element={
-            <ProtectedRoute>
-              <AddPatients />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/patients"
-          element={
-            <ProtectedRoute>
-              <PatientList />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/patients/:id"
-          element={
-            <ProtectedRoute>
-              <PatientDetail />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </Router>
+    <ChakraProvider>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/transporter-dashboard" element={<TransporterDashboard />} />
+          {/* Add other routes as needed */}
+        </Routes>
+      </Router>
+    </ChakraProvider>
   );
-};
+}
 
 export default App;
